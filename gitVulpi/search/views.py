dd31adad232dcd53a04d79310b60daa7687e3bf4
Junior Cesar <juniorclark@gmail.com>
2017-07-17 12:18:59 -0300

    for challenge

---
+++ gitVulpi/search/views.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+import urllib, json
+
+  	
+class SearchGetView(APIView):
+
+
+	def get(self, request):
+		
+		language = request.GET.get('language')
+		location = request.GET.get('location')
+
+		filter_language = self.stripparameter(language)
+		filter_location = self.stripparameter(location)
+
+		get_return_github = self.conectionapi(filter_language, filter_location)
+		newjson = self.iteration_json(get_return_github['items'])
+		return Response({"data": newjson})
+
+
+	def stripparameter(self, param):
+
+		strip_variable = param.replace(" ", "-")
+		return strip_variable
+
+
+	def conectionapi(request, language, location):
+
+		url = "https://api.github.com/search/users?q=language:"+language+"+location:"+location+"&type=Users"
+		
+		response = urllib.urlopen(url)
+		data = json.loads(response.read())
+		return data
+
+
+	def iteration_json(self, data):
+       
+		datajson = []
+		for name in data:
+			login_arr = {}
+			login_arr['login'] = name['login']
+			login_arr['foto'] = name['avatar_url']
+			login_arr['classificacao'] = self.is_jedi(name['score'])
+			datajson.append(login_arr)
+		return datajson
+
+
+	def is_jedi(self, score):
+		if score >= 3.5:
+			get_type = "cavaleiro jedi"
+		else:
+		   get_type = "aprendiz jedi"
+
+		return get_type
